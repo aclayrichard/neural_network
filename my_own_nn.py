82df439b41e89c9574b6bb9ddec8a2abb452fb9e
Clay Richard <aclayrichard@clayrichard.attlocal.net>
2020-06-05 12:08:59 -0500

    got the training arrays at least (not done)

--- my_own_nn.py
+++ my_own_nn.py
@@ -3,15 +3,47 @@ import numpy as np
 from matplotlib import pyplot as plt
 import yfinance as yf
 
-def scale_inputs(inputs):
-    global range_of_inputs
-    range_of_inputs = np.max(inputs) - np.min(inputs)
-    return (inputs - np.min(inputs)) / range_of_inputs
+def scale_inputs(inputs, outputs):
 
-def scale_outputs(outputs):
-    global range_of_outputs
-    range_of_outputs = np.max(outputs) - np.min(outputs)
-    return (outputs - np.min(outputs)) / range_of_outputs
+    if np.max(inputs) >= np.max(outputs):
+        if np.min(inputs) <= np.min(outputs):
+
+            range_inputs = np.max(inputs) - np.min(inputs)
+            return (inputs / range_inputs) - (np.min(inputs) / range_inputs)
+        else:
+
+            range_inputs = np.max(inputs) - np.min(outputs)
+            return (inputs / range_inputs) - (np.min(outputs) / range_inputs)
+    else: 
+        if np.min(inputs) <= np.min(outputs):
+
+            range_inputs = np.max(outputs) - np.min(inputs)
+            return (inputs / range_inputs) - (np.min(inputs) / range_inputs)
+        else:
+
+            range_inputs = np.max(outputs) - np.min(outputs)
+            return (inputs / range_inputs) - (np.min(outputs) / range_inputs)
+
+def scale_outputs(inputs, outputs):
+
+    if np.max(inputs) >= np.max(outputs):
+        if np.min(inputs) <= np.min(outputs):
+
+            range_outputs = np.max(inputs) - np.min(inputs)
+            return (outputs / range_outputs) - (np.min(inputs) / range_outputs)
+        else:
+
+            range_outputs = np.max(inputs) - np.min(outputs)
+            return (outputs / range_outputs) - (np.min(outputs) / range_outputs)
+    else:
+        if np.min(inputs) <= np.min(outputs):
+
+            range_outputs = np.max(outputs) - np.min(inputs)
+            return (outputs / range_outputs) - (np.min(inputs) / range_outputs)
+        else:
+
+            range_outputs = np.max(outputs) - np.min(outputs)
+            return (outputs / range_outputs) - (np.min(outputs) / range_outputs)
 
 def scale(array):
     return (array - np.min(array)) / (np.max(array) - np.min(array))
@@ -55,51 +87,75 @@ class NeuralNetwork:
         return self.sigmoid(np.dot(new_data, self.weights))
 
 
-data = yf.download('AAPL', start='2010-04-10', stop='2020-05-27')
+data = yf.download('AAPL', start='2010-04-10', stop='2020-06-04')
 data = np.array(data['High'])
 scaled_data = scale(data)
-# print(len(scaled_data))
+# print(scaled_data)
+# print(len(data))
 
-def create_train_input(scaled_data):
+def create_train_input(data):
 
     train_input = []
     train_size = 100
-    for i in range(train_size,len(scaled_data)):
-        input_layer = scaled_data[i-train_size:i]
-        train_input = np.append(train_input,input_layer)
+    for i in range(train_size,len(data)):
+        
+        train_input.append(data[i-train_size:i])
         
     return train_input
 
-def create_train_output(scaled_data):
+def create_train_output(data):
 
-    train_size = 100
     train_output = []
-    for i in range(train_size, len(scaled_data)):
-        train_output = np.append(train_output, scaled_data[i])
+    train_size = 100
+    for i in range(train_size, len(data)):
+
+        train_output.append(data[i])
 
     return train_output
 
-print(create_train_input(scaled_data))
-print(create_train_output(scaled_data))
+weights = []
+for i in range(100):
+    weights.append(np.random.uniform())
+weights = np.reshape(weights, [-1, 1])
 
+inputs = create_train_input(data)
+outputs = create_train_output(data)
+outputs = np.reshape(outputs, [-1,1])
 
+# print(len(inputs))
+# print(len(outputs))
+# print(len(weights))
+
+scaled_inputs = scale_inputs(inputs, outputs)
+scaled_outputs = scale_outputs(inputs, outputs)
+
+# print(scaled_inputs, scale_outputs)
+
+print(scaled_data[-100:])
 # inputs = np.array([[1, 2, 3],
 #                    [4, 5, 6],
 #                    [7, 8, 9],
 #                    [2, 3, 4]])
 
+# weights = [[.8],[.7],[.4]]
+
 # outputs = np.array([[4], [7], [10], [5]])
 
-# weights = np.array([[.8],[.4,],[.7]])
+# print(inputs)
+# print(outputs)
+# print(weights)
 
-# inputs = scale_inputs(inputs)
-# outputs = scale_outputs(outputs)
+# scaled_inputs = scale_inputs(inputs, outputs)
+# scaled_outputs = scale_outputs(inputs, outputs)
 
-# Go = NeuralNetwork(inputs, outputs, weights)
+# print(scaled_inputs)
+# print(scaled_outputs)
+
+# Go = NeuralNetwork(scaled_inputs, scaled_outputs, weights)
 
 # Go.train()
 
-# test = np.array([[.3,.4,.5]])
+# test = np.array(scaled_data[-100:])
 
 # print(Go.predict(test))
 
